9601b4d71089bb30c23b828f5c7a4f3a9cb9d005
Mario Dickerson <dai.dickerson@gmail.com>
2023-07-13 19:48:16 -0500

    change color

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function(){
+    let status = this.document.getElementById("flightStatus");
+
+    let upButton = this.document.getElementById("up");
+    let downButton = this.document.getElementById("down");
+    let rightButton = this.document.getElementById("right");
+    let leftButton = this.document.getElementById("left");
+
+    let takeoffButton = this.document.getElementById("takeoff");
+    let landButton = this.document.getElementById("landing");
+    let abortButton = thos.document.getElementById("missionAbort");
+
+    upButton.addEventListener("click", function(){
+        prompt("it's working");
+    })
+
+    takeoffButton.addEventListener("click", function(){
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+    });
+})
